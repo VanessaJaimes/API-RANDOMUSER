b80e2f8e8ec62ac78c817d1127aaa6eaa5e7f79f
ladyvane14 <ladyvane1412@gmail.com>
2023-02-14 13:13:39 -0500

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return { 
-      userscopia:[],
+    return {
+      userscopia: [],
       users: [],
       userInput: '',
       passwordInput: ''
@@ -12,23 +12,22 @@ const app = Vue.createApp({
       const rest = await fetch('https://randomuser.me/api/?results=5');
       const data = await rest.json();
       console.log(data);
-      this.users = data.results
-      this.users = data.results
-      console.log(this.users);
+      this.users = data.results;
+      // this.users = data.results
+      // console.log(this.users);
     },
     signIn(e) {
-
-      console.log(this.users);
+      e.preventDefault();
+      //console.log(this.users);
       this.users.forEach(element => {
-          console.log(element);
-          if (element.login.username == this.userInput && element.login.password == this.passwordInput) {
-            location.href = '../tabla.html'
-          }
-      });      
-      
-    },
-    eliminar(){
-     this.userscopia 
+        console.log(element);
+        console.log(element.login.username);
+        console.log(this.userInput);
+        if (element.login.username == this.userInput && element.login.password == this.passwordInput) {
+          window.location.href = '../tabla.html'
+        }
+      });
+
     }
   },
   async mounted() {
